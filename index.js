3f584f46b76a2cd45f25ec014b8cfb593c8db799
Susan Brooks <susantans@icloud.com>
2023-08-14 19:36:47 -0700

    block31

--- index.js
+++ index.js
@@ -5,46 +5,56 @@ const pets = require('./data');
 const express = require('express');
 const app = express();
 
-const PORT = 8080;
+const PORT = 8081;
+
+ // serve up the public folder as static index.html file
+app.use(express.static('public'));
+
 
 // GET - / - returns homepage
-app.get('/', (req, res) => {
-    // serve up the public folder as static index.html file
+app.get('/', (req, res,next) => {
+    // send the index.html file as a response
+    res.sendFile(__dirname + '/public/index.html');
 
 });
 
 // hello world route
-app.get('/api', (req, res) => {
+app.get('/api', (req, res,next) => {
     res.send('Hello World!');
 });
 
 // get all pets from the database
-app.get('/api/v1/pets', (req, res) => {
+app.get('/api/v1/pets', (req, res,next) => {
     // send the pets array as a response
+    res.json(pets);
 
 });
 
 // get pet by owner with query string
-app.get('/api/v1/pets/owner', (req, res) => {
+app.get('/api/v1/pets/owner', (req, res,next) => {
     // get the owner from the request
-
-
+    const owner = req.query.owner;
+    
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
+    res.json(pet);
+
 
 });
 
 // get pet by name
-app.get('/api/v1/pets/:name', (req, res) => {
+app.get('/api/v1/pets/:name', (req, res,next) => {
     // get the name from the request
-
-
+    const name = req.params.name;
+    
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
+    res.json(pet);
+
 
 });
 
